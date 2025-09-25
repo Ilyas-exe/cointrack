4aa61778f27ca4c47f28bc22437f5290eabf7a6f
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-09-25 09:38:14 +0100

    Use registerUser controller in user registration route
    Replaces the placeholder registration route handler with the registerUser controller for handling user registration logic.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser } = require('../controllers/userController');
 
-router.post('/', (req, res) => {
-    res.send('Register Route');
-});
+router.post('/', registerUser);
 
 module.exports = router;
